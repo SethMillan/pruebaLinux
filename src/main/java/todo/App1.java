b23e07b9f7babb44e6ef7123d611af5c15bbb9ef
Seth Millan <millanseth@gmail.com>
2024-04-28 09:03:58 -0600

    Creando repositorio

---
+++ src/main/java/todo/App1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package todo;
+
+/**
+ *
+ * @author seth
+ */
+public class App1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
